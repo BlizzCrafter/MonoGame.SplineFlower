3ab06e182bebce993165a35e61ffc86ee85879d1
Marcel Härtel <m.haertel@gmx.net>
2018-08-21 12:51:46 +0200

    Editor initial setup.

---
+++ MonoGame.SplineFlower.Editor/EditorForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace MonoGame.SplineFlower.Editor
+{
+    partial class FormEditor
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormEditor
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(571, 350);
+            this.Name = "FormEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MonoGame.SplineFlower.Editor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ MonoGame.SplineFlower.Editor/EditorForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonoGame.SplineFlower.Editor
+{
+    public partial class FormEditor : Form
+    {
+        public FormEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}

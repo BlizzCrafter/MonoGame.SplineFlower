4ef71aa2edb60816936786ab37574ff058f42bb4
Marcel Härtel <m.haertel@gmx.net>
2018-08-28 13:57:19 +0200

    Using the Car class in the GameTest. Adding corresponding content files.
    Added SplineFlower BezierSpline.

--- MonoGame.SplineFlower.GameTest/Game1.cs
+++ MonoGame.SplineFlower.GameTest/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.SplineFlower.Content;
+using MonoGame.SplineFlower.Samples;
 
 namespace MonoGame.SplineFlower.GameTest
 {
@@ -9,7 +10,8 @@ namespace MonoGame.SplineFlower.GameTest
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        BezierSpline GetBezierSpline;
+        BezierSpline MyBezierSpline;
+        Car MySplineWalker;
 
         public Game1()
         {
@@ -28,12 +30,18 @@ namespace MonoGame.SplineFlower.GameTest
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            GetBezierSpline = Content.Load<BezierSpline>(@"Spline");
+            MyBezierSpline = Content.Load<BezierSpline>(@"SplineFlower");
+
+            MySplineWalker = new Car();
+            MySplineWalker.CreateSplineWalker(MyBezierSpline, SplineWalker.SplineWalkerMode.Once, 7);
+            MySplineWalker.LoadContent(Content, Content.Load<SpriteFont>(@"GameFont"));
         }
 
         protected override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+
+            MySplineWalker.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -42,7 +50,8 @@ namespace MonoGame.SplineFlower.GameTest
 
             spriteBatch.Begin();
 
-            GetBezierSpline.DrawSpline(spriteBatch);
+            MyBezierSpline.DrawSpline(spriteBatch);
+            MySplineWalker.Draw(spriteBatch);
 
             spriteBatch.End();
 
